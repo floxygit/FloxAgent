961099e50fa1cae52cc1d6df06d787131f1a3b04
floxygit <floxymusic1@gmail.com>
2026-04-30 12:19:42 +0200

    Implement interactive menu in main.sh
    Added a menu loop for user interaction with options.

--- code/main.sh
+++ code/main.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 clear
 echo "Loading..."
 sleep 1
@@ -7,3 +9,24 @@ echo "Installing wget and curl."
 sleep 1
 apt install curl wget -y
 clear
+
+while :
+do
+  echo "FloxAgent V1.0 Menu"
+  echo
+  echo "[1] Providers"
+  echo "[0] Exit"
+  printf "Auswahl: "
+  read -r choice
+  case "$choice" in
+    1)
+      clear
+      ;;
+    0)
+      exit 0
+      ;;
+    *)
+      clear
+      ;;
+  esac
+done
